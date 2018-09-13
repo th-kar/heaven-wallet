2b72d19e71ff93bcba3c09f353f73d84cc72af7e
Thanos Karagiannis <than.karagiannis@gmail.com>
2018-09-13 12:20:04 +0300

    Initial commit

---
+++ src/main/java/com/tk/fin/optimizedwallet/OptimizedWalletApplication.java
@@ -0,0 +1,12 @@
+package com.tk.fin.optimizedwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizedWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimizedWalletApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tk/fin/optimizedwallet/OptimizedWalletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tk.fin.optimizedwallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OptimizedWalletApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
